0ab50bb109714b2a712a2996a5adda21f5e1a9a4
morilab-uosaka <wangzhe@epc.osaka-u.ac.jp>
2025-08-27 03:54:16 +0000

    Deploying to gh-pages from @ morilab-uosaka/morilab-uosaka.github.io@c2b9c37ad9a319e04e67fa149b78e0da3637f945 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -66,42 +66,7 @@ ninja.data = [{
             },},{id: "news-hpを開設しました",
           title: 'HPを開設しました！',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-量子化学において芳香族性評価手法の改良",
-          title: '量子化学において芳香族性評価手法の改良',
-          description: "既存の芳香族性評価手法が系や設定に依存してブレやすい問題に対し，普遍的に頑健な指標へと再設計する",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{
+          section: "News",},{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
